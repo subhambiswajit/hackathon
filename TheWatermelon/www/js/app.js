5ca0491ad922a4d6fc25a6148933b7c1e87fb001
subhambiswajit <digu35@gmail.com>
2016-03-22 22:13:11 +0530

    profile script created

--- TheWatermelon/www/js/app.js
+++ TheWatermelon/www/js/app.js
@@ -39,60 +39,40 @@ angular.module('starter', ['ionic','starter.controllers'])
       }               
                }) */
 		       
-        .state('Page1',{
-               url: '/Page1',
-               templateUrl:'Page1.html',
-               controller:'Ctrl'
-               })
-    .state('Side',{
-               url: '/Side',
-               templateUrl:'Side.html',
-         abstract: true,
-               controller:'Ctrl'
-               })
-        .state('Side.dash',{
-               url:'/dash',
-         views: {
-        'appContent' :{
-          templateUrl: 'dash.html',
-          controller : 'Ctrl'
-        }
-      }               
-               })
-        .state('Page3',{
-               url: '/Page3',
-               templateUrl:'Page3.html',
-               controller:'Ctrl'
-               })
-    .state('Page4',{
-               url: '/Page4',
-               templateUrl:'Page4.html',
-               controller:'Ctrl'
-               })
-    .state('Page5',{
-               url: '/Page5',
-               templateUrl:'Page5.html',
-               controller:'Ctrl'
-               })
-    .state('Page6',{
-               url: '/Page6',
-               templateUrl:'Page6.html',
-               controller:'Ctrl'
-               })
-    .state('Page7',{
-               url: '/Page7',
-               templateUrl:'Page7.html',
-               controller:'Ctrl'
-               })
-    .state('signup',{
-               url: '/signup',
-               templateUrl:'Signup.html',
-               controller:'Ctrl'
-               })
-    .state('dashboard',{
-                url:'/dashboard',
-                templateUrl:'dashboard.html',
-                controller:'Ctrl'
-                })
+              .state('Page1',{
+                     url: '/Page1',
+                     templateUrl:'Page1.html',
+                     controller:'Ctrl'
+                     })
+              .state('Side',{
+                     url: '/Side',
+                     templateUrl:'Side.html',
+               abstract: true,
+                     controller:'Ctrl'
+                     })
+              .state('Side.dash',{
+                     url:'/dash',
+                     views: {
+                    'appContent' :{
+                      templateUrl: 'dash.html',
+                      controller : 'Ctrl'
+                    }
+                    }               
+                     })
+              .state('signup',{
+                     url: '/signup',
+                     templateUrl:'Signup.html',
+                     controller:'Ctrl'
+                     })
+              .state('dashboard',{
+                      url:'/dashboard',
+                      templateUrl:'dashboard.html',
+                      controller:'Ctrl'
+                      })
+              .state('profile',{
+                      url:'/profile',
+                      templateUrl:'profile.html',
+                      comtroller:'Ctrl'
+                      })
  $urlRouterProvider.otherwise('/Page1');
     });
